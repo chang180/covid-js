9bdc72f1038d2ae36483dde14e7fd2e8e3bf19b2
張建文 <chang180@gmail.com>
2021-08-21 22:46:50 +0800

    改用php-curl-class方式取得資料

--- curlget.php
+++ curlget.php
@@ -1,81 +1,22 @@
 <?php
-//初始化
 
-$ch = curl_init();
-//設定選項，包括URL
+require __DIR__ . '/vendor/autoload.php';
 
-// curl_setopt($ch, CURLOPT_URL, "https://od.cdc.gov.tw/eic/Weekly_Age_County_Gender_19CoV.csv");
-curl_setopt($ch, CURLOPT_URL, "https://od.cdc.gov.tw/eic/Weekly_Age_County_Gender_19CoV.json");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_HEADER, 0);
+use Curl\Curl;
 
-//執行並獲取HTML文件內容
-$output = curl_exec($ch);
+$curl = new Curl();
+$curl->get('https://od.cdc.gov.tw/eic/Weekly_Age_County_Gender_19CoV.json');
 
-//釋放curl控制代碼
-curl_close($ch);
-
-//列印獲得的資料
-// echo "<script>console.log(" . $output . ")</script>";
-
-//去除縣市為空值及2021年之前筆數
-$remove_empty = [];
-foreach (json_decode($output) as $key => $val) {
-    // var_dump($val->{'縣市'},"<br>");
-    if ($val->{'縣市'} != "空值" && $val->{'發病年份'} > 2020) $remove_empty[] = $val;
+if ($curl->error) {
+    echo 'Error: ' . $curl->errorCode . ': ' . $curl->errorMessage . "\n";
+} else {
+    // echo 'Response:' . "\n";
+    // var_dump(json_encode($curl->response));
+    // 去除縣市為空值及2021年之前筆數
+    $remove_empty = [];
+    foreach (json_decode(json_encode($curl->response)) as $key => $val) {
+        // var_dump($val->{'縣市'},"<br>");
+        if ($val->{'縣市'} != "空值" && $val->{'發病年份'} > 2020) $remove_empty[] = $val;
+    }
+    echo json_encode($remove_empty);
 }
-// print_r($remove_empty);
-
-// 分別處理各縣市按週別加總之發病人數
-// $new_array = json_encode([
-//     'city'=>'',
-//     'county'=>'',
-//     'year'=>'',
-//     'week'=>0,
-//     'number'=>0
-// ]);
-
-//要存檔時使用，欄位可換為英文，較容易操作
-// $new_array=[];
-
-// foreach ($remove_empty as $k => $v) {
-//     $vFlag = $v->{'縣市'} . $v->{'鄉鎮'} . $v->{'發病年份'} . $v->{'發病週別'};
-
-//     if (isset($new_array[$vFlag])) {
-//         // $new_array[$vFlag]['number'] += $v['確定病例數'];
-//         $new_array['number'] += $v['確定病例數'];
-//         // $new_array['city'] = $v->{'縣市'};
-//         // $new_array['county'] = $v->{'鄉鎮'};
-//         // $new_array['year'] = $v->{'發病年份'};
-//         // $new_array['week'] = $v->{'發病週別'};
-//         // $new_array['number'] = $v->{'確定病例數'};
-//     } else {
-//         // $new_array->{'city'} = $v->{'縣市'};
-//         // $new_array->{'county'} = $v->{'鄉鎮'};
-//         // $new_array->{'year'} = $v->{'發病年份'};
-//         // $new_array->{'week'} = $v->{'發病週別'};
-//         // $new_array->{'number'} = $v->{'確定病例數'};
-//         $new_array['city'] = $v->{'縣市'};
-//         $new_array['county'] = $v->{'鄉鎮'};
-//         $new_array['year'] = $v->{'發病年份'};
-//         $new_array['week'] = $v->{'發病週別'};
-//         $new_array['number'] = $v->{'確定病例數'};
-//     }
-// }
-// var_dump($new_array);
-// print_r(array_values($new_array));
-
-// $strOUT = json_encode($arrOUT);
-// echo $strOUT;
-
-//將資料存入json檔，若json已存在，先刪除再存
-
-// $covid = 'covid.json';
-// if (file_exists($covid)) unlink($covid); //若已存在，則將檔案刪除
-
-// $open = fopen($covid, 'w+');
-// fwrite($open, json_encode($remove_empty));
-// fclose($open);
-// echo "file updated";
-// echo "<script>console.log(" . json_encode($remove_empty) . ")</script>";
-echo json_encode($remove_empty);
